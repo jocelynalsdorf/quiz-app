c322fc7338c9d14bdba2c0163f018dd977113e82
jocelynalsdorf <jfrodsla@gmail.com>
2014-11-27 09:52:44 +0700

    add the quiz question variables and the modal fade in and out

--- js/main.js
+++ js/main.js
@@ -1,12 +1,43 @@
 $(document).ready(function() {
 
 
+/*--- Display information modal box ---*/
+  	$(".what").click(function() {
+    	$(".instructions").fadeIn(1000);
 
+  	});
 
+  	/*--- Hide information modal box ---*/
+  	$("a.close").click(function(){
+  		$(".instructions").fadeOut(1000);
+  	});
 
+/*-----the quiz questions------*/
+var questions = [
+  
+{question: "The Mars Chocolate company does NOT make which of the following candy bars:",
+ choices: ["Twix", "Kit Kat", "M&Ms", "3 Musketeers"],
+ quesNum: 1,
+ correctAns: 1},
 
+ {question: "What candy bar claims to decrease your hunger effectively?",
+ choices: ["Almond Joy","Payday","M&Ms", "Snickers"],
+ quesNum: 2,
+ correctAns: 3},
 
+{question: "Which of the following do not have a peanut butter version available?",
+ choices: ["Twix", "Kit Kat", "Snickers", "Milky Way"],
+ quesNum: 3,
+ correctAns: 3},
 
+{question: "Which candy has a slogan that invites you to \"taste\" an element of nature:",
+ choices: ["Starburst", "Twizzlers", "Jolly Rancher", "Skittles"],
+ quesNum: 4,
+ correctAns: 3},
+
+ ];
+
+/*---------------------*/
 
 
 
