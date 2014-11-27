71b20c50048d518f78e0275d5ea19e883e974c15
jocelynalsdorf <jfrodsla@gmail.com>
2014-11-27 12:11:17 +0700

    hooked up alert to test comparing answers on radio click, setting up main updateCount function

--- js/main.js
+++ js/main.js
@@ -12,35 +12,58 @@ $(document).ready(function() {
   		$(".instructions").fadeOut(1000);
   	});
 
-/*-----the quiz questions------*/
-var questions = [
+
+
+
+
+	/*-----the quiz questions------*/
+	var questions = [
   
-{question: "The Mars Chocolate company does NOT make which of the following candy bars:",
- choices: ["Twix", "Kit Kat", "M&Ms", "3 Musketeers"],
- quesNum: 1,
- correctAns: 1},
+	{question: "The Mars Chocolate company does NOT make which of the following candy bars:",
+	 choices: ["Twix", "Kit Kat", "M&Ms", "3 Musketeers"],
+	 quesNum: 1,
+	 correctAns: 1},
 
- {question: "What candy bar claims to decrease your hunger effectively?",
- choices: ["Almond Joy","Payday","M&Ms", "Snickers"],
- quesNum: 2,
- correctAns: 3},
+	 {question: "What candy bar claims to decrease your hunger effectively?",
+	 choices: ["Almond Joy","Payday","M&Ms", "Snickers"],
+	 quesNum: 2,
+	 correctAns: 3},
 
-{question: "Which of the following do not have a peanut butter version available?",
- choices: ["Twix", "Kit Kat", "Snickers", "Milky Way"],
- quesNum: 3,
- correctAns: 3},
+	{question: "Which of the following do not have a peanut butter version available?",
+	 choices: ["Twix", "Kit Kat", "Snickers", "Milky Way"],
+	 quesNum: 3,
+	 correctAns: 3},
 
-{question: "Which candy has a slogan that invites you to \"taste\" an element of nature:",
- choices: ["Starburst", "Twizzlers", "Jolly Rancher", "Skittles"],
- quesNum: 4,
- correctAns: 3},
+	{question: "Which candy has a slogan that invites you to \"taste\" an element of nature:",
+	 choices: ["Starburst", "Twizzlers", "Jolly Rancher", "Skittles"],
+	 quesNum: 4,
+	 correctAns: 3},
 
- ];
+	 ];
 
-/*---------------------*/
-
-
+	/*-----global var----------------*/
 
 
+	 var numCorrect = 0;
+	 var currentQuestion = 0;
 
-});
+
+	 /*-----get radio input value on click of submit btn----------------*/
+
+	 $(".submit-btn").on("click", function() {
+	 //alert("hey");
+	 updateCount();
+	 currentQuestion++;
+	 });
+
+	
+
+	function updateCount() {
+		var answer = $("input[type='radio']:checked").val();
+		if(answer == questions[currentQuestion].correctAns) {
+			alert("yep");
+		
+		}
+	}
+
+	});
